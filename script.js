fcafef94613e29db5c5d405af10e6122b8e6ce9b
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-11-17 22:53:50 -0300

    part 1 completed

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("Ola mundo")
+// Changing Outline Color:
+
+const nameOutlineColor =  document.getElementById('shopName');
+const addressOutlineColor =  document.getElementById('shopAddress');
+const openingOutlineColor = document.getElementById('openingTime')
+const closingOutlineColor = document.getElementById('closingTime')
+
+nameOutlineColor.addEventListener('focus', (event) => {
+    event.target.style.outlineColor = 'purple';
+});
+
+nameOutlineColor.addEventListener('blur', (event) => {
+    event.target.style.borderColor = '';
+    console.log("Shop name: " +event.target.value)
+});
+
+addressOutlineColor.addEventListener('focus', (event) => {
+    event.target.style.outlineColor = 'purple';
+});
+
+addressOutlineColor.addEventListener('blur', (event) => {
+    event.target.style.borderColor = '';
+    console.log("Shop address: " +event.target.value)
+});
+
+openingOutlineColor.addEventListener('focus', (event) => {
+    event.target.style.outlineColor = 'purple';
+});
+
+openingOutlineColor.addEventListener('blur', (event) => {
+    event.target.style.borderColor = '';
+    console.log("Opéning time:" +event.target.value)
+});
+
+closingOutlineColor.addEventListener('focus', (event) => {
+    event.target.style.outlineColor = 'purple';
+});
+
+closingOutlineColor.addEventListener('blur', (event) => {
+    event.target.style.borderColor = '';
+    console.log("Closing time:" +event.target.value)
+});
+
+// Button function:
+
+const buttonPressed = document.getElementById('submitButton')
+
+buttonPressed.addEventListener('click', (event) => {
+    console.log("preparando envio...")
+})
